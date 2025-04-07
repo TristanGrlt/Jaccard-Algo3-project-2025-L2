9c8d08e48b378b4e158af5951f0cdc49c309a1e4
TristanGrlt <tristan.groult@gmail.com>
2025-04-07 18:33:00 +0200

    Ajout du module stack pour stocker un nombre indéfinie de fichiers en mémoie et dévelopement du mofule opt pour gérer la lecture et stockage des options dur laligne de commande

---
+++ main/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "opt.h"
+
+int main(int argc, char *argv[]) {
+  opt *option = opt_empty();
+  if (option == nullptr) {
+    printf("error no more place");
+    return EXIT_SUCCESS;
+  }
+  opt_create(option, argv, argc);
+  opt_dispose(&option);
+  return EXIT_SUCCESS;
+}
---
+++ opt/opt.h
@@ -0,0 +1,30 @@
+// opt : Module permettant la gestion les options de la ligne de commande en
+// mémorisant leur contenue dans une structure nommée opt.
+
+#ifndef OPT_H
+#define OPT_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+typedef struct opt opt;
+
+void test(void);
+
+// opt_empty : tente d'allouer les ressources nécessaires pour gérer une nouveau
+// gestionnaire d'option initialisé avec les champs initilisé à leur valeur de
+// départ. Renvoie un pointeur nul en cas de dépassement de capacité. Renvoie
+// sinon un pointeur vers le contrôleur associé
+// à l'option.
+opt *opt_empty(void);
+
+// opt_dispose : sans effet si *optr est un pointeur nul. Libère sinon les
+// ressources allouées à la gestion de l'option associée à *optr puis affecte
+// nul à *optr.
+void opt_dispose(opt **optr);
+
+// opt_create : tente de de remplire le gestionnaire d'options avec les
+// arguments contenue dans le tableau argv[] de longeur argc. Renvoit un nombre
+// strictement négatif en cas d'erreur de tout type une valeur nulle sinon.
+int opt_create(opt *p, char *argv[], int argc);
+#endif
