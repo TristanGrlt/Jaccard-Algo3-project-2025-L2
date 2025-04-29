c06f3b52f62ad750669d41fa3834ace53c264459
TristanGrlt <tristan.groult@gmail.com>
2025-04-29 23:12:17 +0200

    Correction des spécifications de tous nos modules. Ajout d'un ip.h pour opt. Ajout d'information supléméntaire et de lexique pour nos module afin de bien les simplifiers et spécifiait

---
+++ opt/opt_ip.h
@@ -0,0 +1,13 @@
+//  opt_ip.h : précisions sur l'implantation du module opt.
+
+//  L'implantation recoure à une structure pour stocker les options lue
+
+
+//  opt_empty : temps constant ; espace constant.
+//  opt_dispose : temps constant ; espace constant.
+//  opt_create : temps linaire en le nombre d'argument lue ; espace constant.
+//  opt_get_files : temps constant ; espace constant.
+//  opt_get_nb_files : temps constant ; espace constant.
+//  opt_get_word_max_lenght : temps constant ; espace constant.
+//  opt_get_is_blank : temps constant ; espace constant.
+//  opt_get_graph_print : temps constant ; espace constant.
